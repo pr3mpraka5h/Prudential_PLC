b44759f8dc5f23ae63a00c4007bb0300fe3613bc
pr3mpraka5h <premprakash.lakshmanan@gmail.com>
2023-01-14 14:44:48 +0530

    Updates

--- src/test/java/com/test/PageProperties/GooglePage_Properties.java
+++ src/test/java/com/test/PageProperties/GooglePage_Properties.java
@@ -6,9 +6,6 @@ public class GooglePage_Properties {
 	
 	
 	public static	By Searchbar=By.xpath("//*[@class='gLFyf']");
-	
 	public static By Prudential_1st_Occurance=By.xpath("//*[@class='v7W49e']/div[1]//*//h3[@class='LC20lb MBeuO DKV0Md']");
 	public static By Autopopulate_list=By.xpath("//*[@class='G43f7e']/li");
-	
-	//*[@class='erkvQe']/div/ul
 }
--- src/test/java/com/test/constants/EnvironmentConstants.java
+++ src/test/java/com/test/constants/EnvironmentConstants.java
@@ -2,7 +2,7 @@ package com.test.constants;
 
 public class EnvironmentConstants {
 	static public String GoogleURL="https://www.google.com/";
-	static public String Gmail_accesstoken="ya29.a0AX9GBdUJHhzBlPwXv7nk2k5xtHa6kvOsM_RlXxJIv9g55D_bDq_JtApqFNvNXb9pvXn8ebFFQiShO_yCpPvTIgPOMe5WkH7myOro7h_dU5on96UeVF2DBlCnGNtWStyHplspNuhaXAnZ8vjgjjUuzcXyuLDRhwaCgYKAS8SARISFQHUCsbCAO-EhpxGpBhHZZPrcq38Rw0165";
+	static public String Gmail_accesstoken="ya29.a0AX9GBdVH4s_jafQWcmvLNl6HEMv_5aUOZ5_1-6_CDyX0rWQvc28afFTwZRmcGBaYUX5smKl8IEbPdy7jmyNMqUQRRLH1RhO7AAlJIcF9bcW0U1afqp0AWNzU8mHZlXdAaeKDr7PM_ah1jpcQ-59pvSY4h1MAqgaCgYKATISARISFQHUCsbCF3rAXRYpYKg2vOsTAxANvw0165";
 	static public String Gmail_URI="https://gmail.googleapis.com/gmail/v1/users/";
 	static public String User_ID="prakash.hacker@gmail.com";
 
