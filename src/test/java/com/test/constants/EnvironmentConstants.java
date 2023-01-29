7d570ebc25cd6391673f38749260d7c98cc94d55
pr3mpraka5h <premprakash.lakshmanan@gmail.com>
2023-01-29 12:23:44 +0530

    Update EnvironmentConstants.java

--- src/test/java/com/test/constants/EnvironmentConstants.java
+++ src/test/java/com/test/constants/EnvironmentConstants.java
@@ -2,8 +2,8 @@ package com.test.constants;
 
 public class EnvironmentConstants {
 	static public String GoogleURL="https://www.google.com/";
-	static public String Gmail_accesstoken="ya29.a0AX9GBdVH4s_jafQWcmvLNl6HEMv_5aUOZ5_1-6_CDyX0rWQvc28afFTwZRmcGBaYUX5smKl8IEbPdy7jmyNMqUQRRLH1RhO7AAlJIcF9bcW0U1afqp0AWNzU8mHZlXdAaeKDr7PM_ah1jpcQ-59pvSY4h1MAqgaCgYKATISARISFQHUCsbCF3rAXRYpYKg2vOsTAxANvw0165";
-	static public String Gmail_URI="https://gmail.googleapis.com/gmail/v1/users/";
+	static public String Gmail_accesstoken="ya29.a0AVvZVso_BeeUryehJT-Fbz6KdHx-nA9SxGWUU8KK_-Dt4AxDXD3RohUUhzFCTtxS9OdbaQMcqMpX2em3PW0x0YiBdxGfmuHiKg_W48lJWqn8_KNKxsiqnhPWOOt48UhAyYd8OIYbxayrxpk74pmAlHTwLVx2KwaCgYKAdYSARISFQGbdwaIzh454GOL4mdltaMWYzvhfA0165";
+		static public String Gmail_URI="https://gmail.googleapis.com/gmail/v1/users/";
 	static public String User_ID="prakash.hacker@gmail.com";
 
 }
